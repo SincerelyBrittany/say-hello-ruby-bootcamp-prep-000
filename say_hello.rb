47f7fbc6105d780c1ea643537e806c90ad130cfb
Brittany Russell <sincerelybr@gmail.com>
2020-02-25 18:52:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,4 @@
 # Build your say_hello method here
 def say_hello (name="Ruby Programmer")
-  print "Hello #{name}"
+  print "Hello #{name}!"
 end
-
-say_hello
-
-def say_hello(name="Ruby Programmer")
